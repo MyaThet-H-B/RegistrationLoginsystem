d664d5b7e9855870816949c528a0f408a9e829c5
MyaThet-H-B <138380241+MyaThet-H-B@users.noreply.github.com>
2023-07-02 21:26:42 +0630

    RegistrationLoginSystem
    Demo

---
+++ Registration&LoginSystem.py
@@ -0,0 +1,26 @@
+#Register System & Log in System
+email = input("Enter your email ; ") 
+phone_num = int (input("Enter your phone number ; ")) 
+
+password = input(("Enter your password ; "))
+comfirm_password = input("Enter comfirm_password ; ")
+
+username = input("Entet your name ; ")
+loginpassword = input("Enter your password ;")
+
+if comfirm_password == password == loginpassword:
+     print("Your login is sucess ! ")
+else:
+     print("Your login is fail !")
+
+
+     for i in range(30):
+          print("system error")
+     if loginpassword == password:
+          print("final sucess")
+
+     
+
+
+ 
+
